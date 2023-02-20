da6761e06ea83a9ed941b2acb85f824a7e6c69ad
Diana Teper <dianateper333@gmail.com>
2023-02-20 16:52:04 +0200

    Add base player and camera movement

---
+++ Assets/Game/CodeBase/Core/Services/IInputService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game.CodeBase.Core.Services
+{
+    public interface IInputService
+    {
+        event Action OnGunSwitch;
+        event Action OnGunReload;
+        event Action OnFire;
+        event Action OnRotateLeft;
+        event Action OnRotateRight;
+        
+        float GetMouseHorizontalInput();
+        float GetMouseVerticalInput();
+    }
+}
---
+++ Assets/Game/CodeBase/PlayerLogic/PlayerMovementSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.CodeBase.PlayerLogic
+{
+    [Serializable]
+    public class PlayerMovementSettings
+    {
+        [SerializeField] private float _rotationSpeed = 10;
+      
+        public float RotationSpeed => _rotationSpeed;
+    }
+}
