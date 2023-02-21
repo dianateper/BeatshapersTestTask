ba790478d06179da8744113a964456b50977f016
Diana Teper <dianateper333@gmail.com>
2023-02-21 09:44:45 +0200

    Add enemy spawning logic
    (cherry picked from commit a480ea05b0e50b0cf27ee6c040288b1471c3ea73)

---
+++ Assets/Game/CodeBase/EnemyLogic/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game.CodeBase.EnemyLogic
+{
+    public class Enemy : MonoBehaviour
+    {
+        private Transform _target;
+        private Vector3 _direction;
+        private float _speed;
+
+        public void Construct(Transform target, float speed)
+        {
+            _target = target;
+            _speed = speed;
+        }
+
+        private void Update()
+        {
+            if (_target == null) return;
+
+            _direction = _target.transform.position - transform.position;
+            _direction.Normalize();
+            transform.position += _direction * (Time.deltaTime * _speed);
+            transform.LookAt(_target);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Game/CodeBase/EnemyLogic/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Game.CodeBase.PlayerLogic;
+using UnityEngine;
+
+namespace Game.CodeBase.EnemyLogic
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private EnemyFactory _enemyFactory;
+        [SerializeField] private float _spawnDelayInSeconds;
+
+        private Transform _target;
+        private WaitForSeconds _spawnDelay;
+        private bool _isSpawning;
+
+        private void Start()
+        {
+            _target = FindObjectOfType<PlayerBase>().transform;
+            _spawnDelay = new WaitForSeconds(_spawnDelayInSeconds);
+            StartSpawning();
+        }
+
+        private void StartSpawning()
+        {
+            _isSpawning = true;
+            StartCoroutine(CreateEnemy());
+        }
+
+        private void StopSpawning()
+        {
+            StopCoroutine(CreateEnemy());
+        }
+
+        private IEnumerator CreateEnemy()
+        {
+            while (_isSpawning)
+            {
+                _enemyFactory.CreateEnemy(_target);
+                yield return _spawnDelay;
+            }
+        }
+    }
+}
