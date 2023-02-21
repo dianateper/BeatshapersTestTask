c09b8c8cb3867babb8cf0b2e0119329a39b2cad1
Diana Teper <dianateper333@gmail.com>
2023-02-21 13:14:51 +0200

    Add reload ammo coroutine

--- Assets/Game/CodeBase/Weapon/Models/GunModel.cs
+++ Assets/Game/CodeBase/Weapon/Models/GunModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 namespace Game.CodeBase.Weapon.Models
@@ -17,13 +16,17 @@ namespace Game.CodeBase.Weapon.Models
         [SerializeField] private GunId _gunId;
         [SerializeField] private Sprite _gunSprite;
         [SerializeField] private int _maxAmmo;
-        [SerializeField] private float _coolDown;
         [SerializeField] private BulletModel bulletModel;
         [SerializeField] protected int _ammoPerShoot;
+        [SerializeField] private float _coolDownTime;
+        [SerializeField] private float _reloadTime;
 
         private bool _canShoot = true;
         private int _currentAmmo;
-     
+        private MonoBehaviour _monoBehaviour;
+        private WaitForSeconds _coolDownDelay;
+        private WaitForSeconds _reloadDelay;
+
         public Sprite Sprite => _gunSprite;
 
         private void OnEnable()
@@ -32,23 +35,41 @@ namespace Game.CodeBase.Weapon.Models
             _canShoot = true;
         }
 
-        public void Shoot(Vector3 startPosition, Vector3 direction, MonoBehaviour monoBehaviour)
+        public void Construct(MonoBehaviour monoBehaviour)
+        {
+            _monoBehaviour = monoBehaviour;
+            _coolDownDelay = new WaitForSeconds(_coolDownTime);
+            _reloadDelay = new WaitForSeconds(_reloadTime);
+        }
+
+        public void Shoot(Vector3 startPosition, Vector3 direction)
         {
             if (_canShoot && _currentAmmo - _ammoPerShoot >= 0)
             {
                 _canShoot = false;
                 bulletModel.Shoot(startPosition, direction);
                 _currentAmmo -= _ammoPerShoot;
-                monoBehaviour.StartCoroutine(StartCoolDown());
+                _monoBehaviour.StartCoroutine(StartCoolDownCoroutine());
             }
         }
 
-        public void ReloadAmmo() => 
-            _currentAmmo = _maxAmmo;
-
-        private IEnumerator StartCoolDown()
+        public void ReloadAmmo()
         {
-            yield return new WaitForSeconds(_coolDown);
+            _canShoot = false;
+            _monoBehaviour.StopCoroutine(ReloadAmmoCoroutine());
+            _monoBehaviour.StartCoroutine(ReloadAmmoCoroutine());
+        }
+
+        private IEnumerator ReloadAmmoCoroutine()
+        {
+            _currentAmmo = _maxAmmo;
+            yield return _reloadDelay;
+            _canShoot = true;
+        }
+
+        private IEnumerator StartCoolDownCoroutine()
+        {
+            yield return _coolDownDelay;
             _canShoot = true;
         }
     }
