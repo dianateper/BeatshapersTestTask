a480ea05b0e50b0cf27ee6c040288b1471c3ea73
Diana Teper <dianateper333@gmail.com>
2023-02-21 09:44:45 +0200

    Add enemy spawning logic

---
+++ Assets/Game/CodeBase/Common/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.CodeBase.Common
+{
+    public class ObjectPool<T> where T: MonoBehaviour
+    {
+        private List<T> pooledObjets;
+
+        public ObjectPool()
+        {
+            pooledObjets = new List<T>();
+        }
+        
+        public T GetFromPool() 
+        {
+            var result = pooledObjets.FirstOrDefault(o => o.gameObject.activeInHierarchy);
+            if (result != null)
+            {
+                result.gameObject.SetActive(true);
+                pooledObjets.Remove(result);
+            }
+            return result;
+        }
+
+        public void ReturnToPool(T objectToReturn) 
+        {
+            objectToReturn.gameObject.SetActive(false);
+            pooledObjets.Add(objectToReturn);
+        }
+    }
+}
---
+++ Assets/Game/CodeBase/EnemyLogic/EnemyFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Game.CodeBase.Common;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game.CodeBase.EnemyLogic
+{
+    [CreateAssetMenu(fileName = "EnemyFactory", menuName = "Game/EnemyFactory")]
+    public class EnemyFactory : ScriptableObject
+    {
+        [SerializeField] private Enemy _enemyPrefab;
+        [SerializeField] private EnemySettings _enemySettings;
+        private ObjectPool<Enemy> _enemies;
+
+        public EnemyFactory()
+        {
+            _enemies = new ObjectPool<Enemy>();
+        }
+        
+        public Enemy CreateEnemy(Transform target)
+        {
+            var enemy = _enemies.GetFromPool();
+            if (enemy == null)
+            {
+                enemy = Instantiate(_enemyPrefab);
+            }
+
+            enemy.transform.SetPositionAndRotation(GetRandomPosition(_enemySettings.EnemySpawnRadius),
+                Quaternion.identity);
+            enemy.Construct(target, _enemySettings.EnemySpeed);   
+            return enemy;
+        }
+
+        public void Reclaim(Enemy enemy)
+        {
+            _enemies.ReturnToPool(enemy);
+        }
+
+        private Vector3 GetRandomPosition(float radius)
+        {
+            var position = Random.onUnitSphere * radius;
+            position.y = 0;
+            return position;
+        }
+    }
+
+    [Serializable]
+    public class EnemySettings
+    {
+        [SerializeField] private float _enemySpawnRadius;
+        [SerializeField] private float _enemySpeed;
+        
+        public float EnemySpawnRadius => _enemySpawnRadius;
+        public float EnemySpeed => _enemySpeed;
+    }
+}
